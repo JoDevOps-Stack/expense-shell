79328a1e5beaf31ca9352733b98513c57e2d66ce
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-11 17:41:54 +0530

    conversation changed

--- mysql.sh
+++ mysql.sh
@@ -42,7 +42,7 @@ VALIDATE $? "enabling mysql server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting mysql server"
 
-mysql -h mysql.daws82s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
+mysql -h mysql.daws82.click -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
