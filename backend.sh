45062f7c3878c22a9af5a851fcb423db2b8db73d
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-15 12:33:03 +0530

    conversation changed

--- backend.sh
+++ backend.sh
@@ -43,6 +43,16 @@ VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing NodeJS"
 
+id expense &>>$LOG_FILE_NAME
+if [$? -ne 0]
+then
+ useradd expense &>>$LOG_FILE_NAME
+ VALIDATE $? "Adding expense user"
+ else
+ echo -e "expense user already exists....$Y skipping "
+ 
+ fi
+
 useradd expense &>>$LOG_FILE_NAME    
 VALIDATE $? "Adding expense user"
 
