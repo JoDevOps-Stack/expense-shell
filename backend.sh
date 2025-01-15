adde2edd5e345fc7e88ebbf1f0c689ab26e20681
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-15 12:21:22 +0530

    conversation changed

--- backend.sh
+++ backend.sh
@@ -31,15 +31,6 @@ CHECK_ROOT(){
 
 echo "Script started executing at:: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-CHECK_ROOT(){
-if [ $USERID -ne 0 ]
-    then
-        echo "ERROR:: You must have sudo access to execute this script"
-        exit 1 #other than 0
-    fi
-}
-
-echo "Script started executing at:: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
@@ -52,14 +43,8 @@ VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing NodeJS"
 
-id expense &>>$LOG_FILE_NAME
-if [ $? -ne 0 ]
-then
-    useradd expense &>>$LOG_FILE_NAME
-    VALIDATE $? "Adding expense user"
-else
-    echo -e "expense user already exists ... $Y SKIPPING $N"
-fi
+useradd expense &>>$LOG_FILE_NAME    
+VALIDATE $? "Adding expense user"
 
 mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating app directory"
@@ -68,7 +53,6 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading backend"
 
 cd /app
-rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzip backend"
@@ -83,7 +67,7 @@ cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.serv
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.daws82.click -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+mysql -h database.daws82.click -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Setting up the transactions schema and tables"
 
 systemctl daemon-reload &>>$LOG_FILE_NAME
